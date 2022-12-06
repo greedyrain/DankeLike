1f287debe08136a477bd22f93b4e01e61cdee50a
greedyrain <greedyrain@163.com>
2022-12-06 17:57:25 +0800

    增加拉席克闪电技能

---
+++ Assets/Scripts/SkillSystem/SkillObject/HitEffect/HitEffect_LightningStorm.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class HitEffect_LightningStorm : BaseSkillObject 
+{
+    Vector3 targetPos;
+    Vector3 ownerPos;
+
+    public bool isInitCompleted;
+    void Update()
+    {
+        UniTask.WaitUntil(() => isInitCompleted).ContinueWith(() =>
+        {
+            if (target.gameObject.activeSelf && owner.gameObject.activeSelf)
+            {
+                targetPos = target.position;
+                transform.position = targetPos + (Vector3.up * 10);
+            }
+        });
+    }
+    private void OnEnable()
+    {
+        isInitCompleted = false;
+        UniTask.WaitUntil(() => isInitCompleted).ContinueWith(() =>
+        {
+            UniTask.Delay((int)(SkillData.duration*1000)).ContinueWith(() =>
+            {
+                target = null;
+                owner = null;
+                isInitCompleted = false;
+                PoolManager.Instance.PushObj(gameObject.name, gameObject);
+            });
+        });
+    }
+
+    public void Init(Transform target, Transform owner)
+    {
+        this.target = target;
+        this.owner = owner;
+        isInitCompleted = true;
+    }
+}
---
+++ Assets/Scripts/SkillSystem/SkillObject/SkillObject_LightningStorm.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class SkillObject_LightningStorm : BaseSkillObject 
+{
+    //设定target和owner，每一帧的位置都设定味owner的位置，朝向target的位置。
+    //如果target isdead，则把target设定为空,如果owner isdead，则把owner设定为空
+    private Collider[] colls;
+    private async void OnEnable()
+    {
+        await UniTask.WaitUntil(() => initCompleted).ContinueWith(async () =>
+        {
+            Active(owner, SkillData.targetCount);
+            await UniTask.Delay(500).ContinueWith(() => PoolManager.Instance.PushObj(gameObject.name, gameObject));
+        });
+    }
+
+    public async void Active(Transform owner, int count)
+    {
+        //初始位置与owner重合，面向target，
+        Transform hitTarget = FindTarget(owner);
+        if (hitTarget != null)
+        {
+            PoolManager.Instance.GetObj("Prefabs/HitEffectObjects", SkillData.hitEffectName, (obj) =>
+            {
+                obj.GetComponent<HitEffect_LightningStorm>().InitData(SkillData);
+                obj.GetComponent<HitEffect_LightningStorm>().Init(hitTarget, owner);
+            });
+            hitTarget.GetComponent<Enemy>().GetHurt(SkillData.damage);
+            if (count > 0)
+            {
+                await UniTask.Delay(300).ContinueWith(() => Active(hitTarget, count - 1));
+            }
+        }
+    }
+    
+    //把目标和所有者放在laser身上，实时计算目标和所有者之间的位置关系
+    public Transform FindTarget(Transform owner)
+    {
+        colls = Physics.OverlapSphere(owner.position, SkillData.range, targetLayer);
+        if (colls.Length > 0)
+        {
+            int index = Random.Range(0, colls.Length);
+            Transform hitTarget = colls[index].transform;
+            if (hitTarget == owner && colls.Length > 1)
+            {
+                FindTarget(owner);
+            }
+            else if (hitTarget == owner && colls.Length <= 1)
+            {
+                return null;
+            }
+            return hitTarget;
+        }
+        return null;
+    }
+}
