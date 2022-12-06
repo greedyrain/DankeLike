86f76285fc31e9677e7e87cd3daecbaec974b788
greedyrain <greedyrain@163.com>
2022-12-06 17:58:17 +0800

    增加拉席克大招

---
+++ Assets/Scripts/SkillSystem/SkillObject/SkillObject_PulseNova.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class SkillObject_PulseNova : BaseSkillObject
+{
+    private void OnEnable()
+    {
+         UniTask.WaitUntil(() => initCompleted).ContinueWith( () =>
+         {
+             Attack();
+            UniTask.Delay((int)(SkillData.duration * 1000)).ContinueWith(()=>PoolManager.Instance.PushObj(gameObject.name,gameObject));
+        });
+    }
+
+    void Attack()
+    {
+        Collider[] colls = Physics.OverlapSphere(transform.position, SkillData.range, targetLayer);
+        if (colls.Length > 0)
+        {
+            foreach (var enemy in colls)
+            {
+                enemy.GetComponent<Enemy>().GetHurt(SkillData.damage);
+                PoolManager.Instance.GetObj("Prefabs/HitEffectObjects", SkillData.hitEffectName, (obj) =>
+                {
+                    obj.transform.position = enemy.transform.position;
+                    UniTask.Delay((int)(SkillData.duration * 1000)).ContinueWith(()=>PoolManager.Instance.PushObj(obj.name,obj));
+                });
+            }
+        }
+    }
+}
