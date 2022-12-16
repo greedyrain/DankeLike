a68be48c8db3d83f69c5f85f5efab0d3865fc7cb
greedyrain <greedyrain@163.com>
2022-12-16 16:47:30 +0800

    增加血魔大招及萨尔风雷之击技能

---
+++ Assets/Scripts/SkillSystem/SkillObject/SkillObject_Rupture.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class SkillObject_Rupture : BaseSkillObject
+{
+    Enemy targetEnemy;
+    Vector3 lastFramesPos;
+
+    private async void OnEnable()
+    {
+        UniTask.WaitUntil(() => initCompleted).ContinueWith(async () =>
+        {
+            targetEnemy = GetComponentInParent<Enemy>();
+            transform.SetParent(targetEnemy.center);
+            lastFramesPos = transform.position;
+            Action();
+            await UniTask.Delay((int)(SkillData.duration * 1000)).ContinueWith(() => PoolManager.Instance.PushObj(gameObject.name, gameObject));
+        });
+
+    }
+
+    public async void Action()
+    {
+        while (gameObject.activeSelf)
+        {
+            if (transform.position != lastFramesPos)
+            {
+                Debug.Log((transform.position - lastFramesPos).magnitude);
+                targetEnemy.GetHurt((int)((transform.position - lastFramesPos).magnitude * SkillData.damage));
+                lastFramesPos = transform.position;
+            }
+            await UniTask.Delay(200);
+        }
+    }
+}
---
+++ Assets/Scripts/SkillSystem/SkillObject/SkillObject_ThunderStrike.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class SkillObject_ThunderStrike : BaseSkillObject
+{
+    public GameObject area;
+    public int count;
+
+    public Enemy targetEnemy;
+
+    private async void OnEnable()
+    {
+        Debug.Log("OnEnable");
+        await UniTask.WaitUntil(() => initCompleted).ContinueWith(() =>
+        {
+            Debug.Log("OnEnable2");
+            count = SkillData.targetCount;
+            targetEnemy = GetComponentInParent<Enemy>();
+            transform.position = new Vector3(transform.position.x, 3, transform.position.z);
+            area.transform.localScale = new Vector3(SkillData.radius * 2, area.transform.localScale.y, SkillData.radius * 2);
+            Action();
+        });
+    }
+
+
+    public async void Action()
+    {
+        while (count > 0)
+        {
+            Debug.Log(count);
+            area.SetActive(true);
+            Collider[] targets = Physics.OverlapSphere(transform.position, SkillData.radius, targetLayer);
+            if (targets.Length > 0)
+            {
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    targets[i].GetComponent<Enemy>().GetHurt(SkillData.damage);
+                }
+            }
+            if (targetEnemy.isDead)
+            {
+                transform.SetParent(null);
+            }
+            count--;
+            UniTask.Delay(200).ContinueWith(() => area.SetActive(false));
+            if (count <= 0)
+            {
+                PoolManager.Instance.PushObj(gameObject.name, gameObject);
+            }
+            await UniTask.Delay((int)(SkillData.actionInterval * 1000));
+        }
+    }
+}
