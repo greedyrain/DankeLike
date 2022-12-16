e84cef3133fb7a2cda78a7b51e2079d704df5211
greedyrain <greedyrain@163.com>
2022-12-16 10:51:37 +0800

    增加lina龙破斩技能

--- Assets/Scripts/SkillSystem/Deployer/FrontDeployer.cs
+++ Assets/Scripts/SkillSystem/Deployer/FrontDeployer.cs
@@ -9,17 +9,36 @@ public class FrontDeployer : SkillDeployer
     {
         transform.position = player.transform.position;
         transform.rotation = player.transform.rotation;
-        angle = 60f;
-        averageAngle = 60f / (SkillData.count - 1);
-        for (int i = 0; i < SkillData.count; i++)
+        if (SkillData.count == 1)
         {
-            PoolManager.Instance.GetObj("Prefabs/SkillObjects", SkillData.prefabName, (obj) =>
+            transform.forward = player.transform.forward;
+            for (int i = 0; i < SkillData.count; i++)
             {
-                obj.transform.position = transform.position;
-                obj.transform.forward = Quaternion.AngleAxis((-angle / 2) + (i * averageAngle), Vector3.up)*transform.forward;
-                obj.GetComponent<BaseSkillObject>().InitData(SkillData);
-            });
-            await UniTask.Delay((int) (SkillData.disposeInterval * 1000));
+                PoolManager.Instance.GetObj("Prefabs/SkillObjects", SkillData.prefabName, (obj) =>
+                {
+                    obj.transform.position = transform.position;
+                    obj.transform.forward = player.transform.forward;
+                    obj.GetComponent<BaseSkillObject>().InitData(SkillData);
+                });
+            }
         }
+        
+        if (SkillData.count > 1)
+        {
+            angle = 60f;
+            averageAngle = 60f / (SkillData.count - 1);
+            for (int i = 0; i < SkillData.count; i++)
+            {
+                PoolManager.Instance.GetObj("Prefabs/SkillObjects", SkillData.prefabName, (obj) =>
+                {
+                    obj.transform.position = transform.position;
+                    obj.transform.forward = Quaternion.AngleAxis((-angle / 2) + (i * averageAngle), Vector3.up)*transform.forward;
+                    obj.GetComponent<BaseSkillObject>().InitData(SkillData);
+                });
+                await UniTask.Delay((int) (SkillData.disposeInterval * 1000));
+            }
+        }
+        
+
     }
 }
---
+++ Assets/Scripts/SkillSystem/SkillObject/SkillObject_DragonSlave.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class SkillObject_DragonSlave : BaseSkillObject
+{
+    Vector3 originPos;
+    private void OnEnable()
+    {
+        UniTask.WaitUntil(() => initCompleted).ContinueWith(() =>
+        {
+            originPos = transform.position;
+            // transform.rotation =Quaternion.identity; 
+        });
+    }
+
+    private void Update()
+    {
+        UniTask.WaitUntil(() => initCompleted).ContinueWith(() =>
+        {
+            Fly();
+        });
+    }
+
+    public void Fly()
+    {
+        transform.Translate(Vector3.forward*SkillData.throwSpeed*Time.deltaTime);
+        if ((transform.position - originPos).magnitude >= SkillData.range)
+        {
+            PoolManager.Instance.PushObj(gameObject.name, gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy>().GetHurt(SkillData.damage);
+        }
+    }
+}
