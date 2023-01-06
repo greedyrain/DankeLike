b8bb6ab09568ad9ba86bf86b445263a8ca5d0a7a
greedyrain <greedyrain@163.com>
2023-01-06 16:31:31 +0800

    去除武器系统

--- Assets/Scripts/SkillSystem/SkillBehaviour/IDamageOverTime.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamageOverTime
-{
-    public void DamageOverTime(GameObject target);
-    public void CancelDamageOverTime(GameObject target);
-}
